b0aad299727247f3768e8d0a2f16c8893049133d
Dre Ibarra <andres.ibarra0716@gmail.com>
2018-05-22 11:50:09 -0400

    ipask exercise

---
+++ day2/ipask.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+ip = input('Please enter your IP address: ')
+print ('You have entered ' + str(ip) + ' as your IPv4 address')
+
+mac = input ('Please enter your MAC address: ')
+print ('You have entered ' + str(mac) + ' as your mac address')
+
+print('Your IP address is ' + str(ip) + ' , and your MAC address is ' + str(mac) + ' .')
