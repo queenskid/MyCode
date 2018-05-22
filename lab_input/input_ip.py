7ff5a79f89d53f724e97c4e78ab7fa7a3ddcf9c7
Dre Ibarra <andres.ibarra0716@gmail.com>
2018-05-22 16:23:40 -0400

    input ip lab complete

--- lab_input/input_ip.py
+++ lab_input/input_ip.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+# Author: Dre
 
-user_input = input('Please enter an IPv4 address:')
-print("You told me the IPv4 address is:" + user_input)
+# Collect IP address information from a user, and display it back to them.
+    
+## Collect input from the user
+user_input = input('Please enter an IPv4 IP address:') 
+# user_input stores the IPv4 ip address
+    
+## Print out the input collected from the user
+print('You told me the IPv4 address is: ' + str(user_input))
+
+## Collecr input from user for vendor information
+vendor_input = input('What is the vendor\'s name? ')
+print('The vendor\'s name is ' + str(vendor_input) + '.')
