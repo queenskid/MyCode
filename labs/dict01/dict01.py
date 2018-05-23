55b748f3867d2dfbbd51780bc6a1a0991cd732e6
Dre Ibarra <andres.ibarra0716@gmail.com>
2018-05-23 16:50:52 -0400

    Lab15 Complete

---
+++ labs/dict01/dict01.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+    
+## create a dictionary
+switch = {'hostname': 'sw1', 'ip': '10.0.1.1', 'version': '1.2', 'vendor': 'cisco'}
+    
+## display parts of the dictionary
+print( switch['hostname'] )
+print( switch['ip'] )
+    
+## request a 'fake' key
+#print( switch['lynx'] )
+
+## request a 'fake' key with .get() method
+print( "First test - .get()" )
+print(switch.get('lynx'))
+
+print( "Second test - .get()" )
+print(switch.get('lynx', "THE KEY IS IN ANOTHER CASTLE!"))
+
+print( "Third test - .get()" )
+print(switch.get('version'))
+
+print( "Fourth test - .keys()" )
+print(switch.keys())
+
+print( "Fifth test - .values()" )
+print(switch.values())
+
+print( "Sixth test - .pop()" )
+
+# removes this key (and value) pair
+switch.pop('version') 
+
+# notice the value of version is gone
+print( switch.keys() )
+
+# notice the value 1.2
+print( switch.values() ) 
+
+print( "Seventh test - ADD a new value" )
+switch['adminlogin'] = 'karl08'
+print( switch.keys() )
+print( switch.values() )
+
+print( "Eighth test - ADD a new value" )
+switch['password'] = 'qwerty'
+print( switch.keys() )
+print( switch.values() )
+
+
+
