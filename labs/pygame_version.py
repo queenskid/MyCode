7e1f5c73a64f34ebdbd0c5ba5beace74a8894fff
Dre Ibarra <andres.ibarra0716@gmail.com>
2018-05-30 12:32:48 -0400

    working with pygame

---
+++ labs/pygame_version.py
@@ -0,0 +1,9 @@
+
+
+#coding=utf-8
+
+import pygame
+
+print(pygame.version.ver)
+
+print(pygame.version.vernum)
---
+++ labs/secondtets.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+num = 25
+
+print(num)
+print('this is a test to see if Atom is working correctly')
+print('What is your name? ')
+name = input()
