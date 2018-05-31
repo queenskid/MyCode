a039a199ee186b5581440272e5f49b734c3e03bb
Dre Ibarra <andres.ibarra0716@gmail.com>
2018-05-31 16:37:01 -0400

    Lab36 Complete

---
+++ labs/iss/ride_iss.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import urllib.request
+import json
+## Trace the ISS
+majortom = 'http://api.open-notify.org/astros.json'
+
+## Call the webservice
+groundctrl = urllib.request.urlopen(majortom)
+
+## put fileobject into helmet
+helmet = groundctrl.read()
+
+## decode json to python data structure
+helmetson = json.loads(helmet.decode('utf-8'))
+
+## display our pythonic data
+print("\n\nConverted python data")
+print(helmetson)
+
+print("\n\nPeople in Space: ", helmetson['number'])
+people = helmetson['people']
+print(people)
+
+# loop will iterate through the range of the lenght of the list named people.
+for x in range(len(people)):
+    #print('People in space: ' + helmetson['number'])
+    print(people[x]['name'] + ' on the ' + people[x]['craft'])
