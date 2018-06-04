d08fbe6fe46ddf1ef16336a90445a8f658211f2c
Dre Ibarra <andres.ibarra0716@gmail.com>
2018-06-04 16:42:30 -0400

    Lab63 Complete

---
+++ labs/cmdargs/pirate-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+
+args = sys.argv
+print("Username: " + args[0])
+print("Password: " + args[1])
+print("IP Address: " + args[2])
+print("Gateway: " + args[3])
+
+print(args)
