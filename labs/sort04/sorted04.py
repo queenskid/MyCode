78a2178d941e96bc0bc6b2e8f07ccb1c7fc2861b
Dre Ibarra <andres.ibarra0716@gmail.com>
2018-06-06 14:11:55 -0400

    Lab72 Complete

---
+++ labs/sort04/sorted04.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+us_invasion = [{'ip':'10.10.1.2', 'un':'john', 'pw':'allstar'}, {'ip':'10.10.1.3', 'un':'paul', 'pw':'iils20s3'}, {'ip':'10.10.1.4', 'un':'george', 'pw':'hunkydoryzory'}, {'ip':'10.10.1.5', 'un':'stuart', 'pw':'alta3'}, {'ip':'10.10.1.6', 'un':'pete', 'pw':'a8dd827z3'}]
+
+def byUserName(x):
+    return x['un']
+
+listbyusername = sorted(us_invasion, key=byUserName)
+
+print('\nThe list us_invasion looks like: ', us_invasion)
+
+print('\nResult of sorted(us_invasion, key=byUserName): ', listbyusername)
+
+print('\nBut the value of the list us_invasion hasn\'t actually changed: ', us_invasion)
